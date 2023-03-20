be4e4f82b8dc457e77129cbdd513e6cab4a0d58d
조은채 <83952073+whdmsco010@users.noreply.github.com>
2023-03-20 22:57:15 +0900

    진실을 말하는 자와 살인자 찾기 게임

---
+++ Assets/Script/Scripts_ec/MissionGame/Select_S.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+public class Select_S : MonoBehaviour
+{
+
+    public void check(InputField f) //InputField를 f로 선언
+    {
+        if (f.text == "모에모에큥")
+        {
+            Debug.Log("Success!");
+        }
+        else
+        {
+            Debug.Log("Retry");
+        }
+    }
+}
