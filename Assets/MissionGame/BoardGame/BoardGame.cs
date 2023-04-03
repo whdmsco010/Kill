e42873931e430507f92dcd3fa0ba48f37874c35f
leedoyun0314 <dlehdbs01@naver.com>
2023-04-03 17:35:13 +0900

    보드게임 최종

--- Assets/MissionGame/BoardGame/BoardGame.cs
+++ Assets/MissionGame/BoardGame/BoardGame.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class BoardGame : MonoBehaviour
 {
-    //public GameObject move;
+    public GameObject success;
     private Coroutine coroutine;
 
     public RectTransform Player;
@@ -41,8 +41,11 @@ public class BoardGame : MonoBehaviour
 
     public void MovePlayer(int count)
     {
-
-        if (count == 1)
+        if (count == 0)
+        {
+            Player.anchoredPosition = new Vector2(-327.9f, -141.1f);
+        }
+        else if (count == 1)
         {
             Player.anchoredPosition = new Vector2(-267.9f, -141.1f);
         }
@@ -55,7 +58,7 @@ public class BoardGame : MonoBehaviour
             Player.anchoredPosition = new Vector2(-147.9f, -141.1f);
             player_count = 4;
 
-            coroutine = StartCoroutine(WaitAndPrint(2.0f, player_count));
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 4)
         {
@@ -66,14 +69,14 @@ public class BoardGame : MonoBehaviour
             Player.anchoredPosition = new Vector2(-27.9f, -141.1f);
             player_count = 7;
 
-            coroutine = StartCoroutine(WaitAndPrint(2.0f, player_count));
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 6)
         {
             Player.anchoredPosition = new Vector2(32.09998f, -141.1f);
             player_count = 4;
 
-            coroutine = StartCoroutine(WaitAndPrint(2.0f, player_count));
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 7)
         {
@@ -84,14 +87,14 @@ public class BoardGame : MonoBehaviour
             Player.anchoredPosition = new Vector2(152.1f, -141.1f);
             player_count = 16;
 
-            coroutine = StartCoroutine(WaitAndPrint(2.0f, player_count));
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 9)
         {
             Player.anchoredPosition = new Vector2(212.0999f, -141.1f);
             player_count = 7;
 
-            coroutine = StartCoroutine(WaitAndPrint(2.0f, player_count));
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 10)
         {
@@ -102,15 +105,21 @@ public class BoardGame : MonoBehaviour
             Player.anchoredPosition = new Vector2(332.0999f, -141.1f);
             player_count = 14;
 
-            coroutine = StartCoroutine(WaitAndPrint(2.0f, player_count));
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 12)
         {
             Player.anchoredPosition = new Vector2(332.1f, -81.1f);
+            player_count = 14;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 13)
         {
             Player.anchoredPosition = new Vector2(332.1f, -21.1f);
+            player_count = 10;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 14)
         {
@@ -119,6 +128,9 @@ public class BoardGame : MonoBehaviour
         else if (count == 15)
         {
             Player.anchoredPosition = new Vector2(212.1f, -21.1f);
+            player_count = 10;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 16)
         {
@@ -127,10 +139,16 @@ public class BoardGame : MonoBehaviour
         else if (count == 17)
         {
             Player.anchoredPosition = new Vector2(92.1f, -21.1f);
+            player_count = 0;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 18)
         {
             Player.anchoredPosition = new Vector2(32.1f, -21.1f);
+            player_count = player_count + dice_count;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 19)
         {
@@ -139,22 +157,37 @@ public class BoardGame : MonoBehaviour
         else if (count == 20)
         {
             Player.anchoredPosition = new Vector2(-87.89999f, -21.1f);
+            player_count = 30;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 21)
         {
             Player.anchoredPosition = new Vector2(-147.9f, -21.1f);
+            player_count = 26;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 22)
         {
             Player.anchoredPosition = new Vector2(-207.9f, -21.1f);
+            player_count = 19;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 23)
         {
             Player.anchoredPosition = new Vector2(-267.9f, -21.1f);
+            player_count = 26;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 24)
         {
             Player.anchoredPosition = new Vector2(-327.9f, -21.1f);
+            player_count = 19;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 25)
         {
@@ -167,10 +200,16 @@ public class BoardGame : MonoBehaviour
         else if (count == 27)
         {
             Player.anchoredPosition = new Vector2(-267.9f, 98.90002f);
+            player_count = 25;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 28)
         {
             Player.anchoredPosition = new Vector2(-207.9f, 98.90002f);
+            player_count = 29;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 29)
         {
@@ -183,14 +222,23 @@ public class BoardGame : MonoBehaviour
         else if (count == 31)
         {
             Player.anchoredPosition = new Vector2(-27.9f, 98.90002f);
+            player_count = player_count + dice_count;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 32)
         {
             Player.anchoredPosition = new Vector2(32.09998f, 98.90002f);
+            player_count = 0;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 33)
         {
             Player.anchoredPosition = new Vector2(92.09998f, 98.90002f);
+            player_count = 29;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 34)
         {
@@ -199,14 +247,21 @@ public class BoardGame : MonoBehaviour
         else if (count == 35)
         {
             Player.anchoredPosition = new Vector2(212.0999f, 98.90002f);
+            player_count = 30;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count == 36)
         {
             Player.anchoredPosition = new Vector2(272.0999f, 98.90002f);
+            player_count = 14;
+
+            coroutine = StartCoroutine(WaitAndPrint(1.5f, player_count));
         }
         else if (count >= 37)
         {
             Player.anchoredPosition = new Vector2(332.0999f, 98.90002f);
+            success.SetActive(true);
         }
     }
 }
